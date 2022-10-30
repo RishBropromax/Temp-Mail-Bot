d9c674c7a5b510e25022aadee23b9cd581c64bbb
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-10-30 14:22:50 +0530

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,36 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+  
+ # Home Button
+    home_buttons = [
+        [InlineKeyboardButton("Emo Bot Devolopers", url="https://t.me/EmoBotDevolopers")],
+    ] 
+  
+  HELP = """
+
+- Send Me /tempmail And I Can Create Your Temp Mail
+- Send Me /mailbox  And you Can Visit Your Mails
+- Send Me /mail You can Viwe Your Mail
+- Send Me /domains You can Visit My All domain Names
+
+🪄Powerd By [Emo Network](t.me/EmoBotDevolopers)
+    """
+
+    # About Message
+    ABOUT = """
+**About This Bot** 
+A Telegram Temp Mail Bot by @ImRishmika
+
+🪄Powerd By : @EmoBotDevolopers
+
+🍁Framework : [Pyrogram](docs.pyrogram.org)
+
+🍁Language : [Python](www.python.org)
+
+🍁Developer : @ImRishmika
+
+🖥️Host Sever : Heroku
+
+    """
